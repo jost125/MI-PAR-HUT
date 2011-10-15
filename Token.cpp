ae48d928d711dd9169ead8443b9f1ce0d66caa34
Jan Machala <jan.machala@email.cz>
2011-10-15 17:30:00 +0200

    Added Token

---
+++ Token.cpp
@@ -0,0 +1,24 @@
+/* 
+ * File:   Token.cpp
+ * Author: honza
+ * 
+ * Created on October 15, 2011, 4:58 PM
+ */
+
+#include "Token.h"
+
+Token::Token(const Coordinate & coordinate, const int price) : coordinate(coordinate) {
+	this->coordinate = coordinate;
+	this->price = price;
+}
+
+Token::~Token() {
+}
+
+Coordinate Token::getCoorinate() const {
+	return this->coordinate;
+}
+
+int Token::getPrice() const {
+	return this->price;
+}
---
+++ Token.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   Token.h
+ * Author: honza
+ *
+ * Created on October 15, 2011, 4:58 PM
+ */
+
+#include "Coordinate.h"
+
+#ifndef TOKEN_H
+#define	TOKEN_H
+
+class Token {
+public:
+    Token(const Coordinate & coordinate, const int price);
+    Coordinate getCoorinate() const;
+    int getPrice() const;
+    ~Token();
+private:
+    Coordinate coordinate;
+    int price;
+};
+
+#endif	/* TOKEN_H */
+
