a0b04c363edc39cc0d1655a40f81199bd20e9f41
Jan Machala <jan.machala@email.cz>
2011-11-12 17:07:51 +0100

    Added useful debug macro

---
+++ Debug.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   Debug.h
+ * Author: honza
+ *
+ * Created on November 12, 2011, 4:33 PM
+ */
+
+#ifndef DEBUG_H
+#define	DEBUG_H
+
+#ifdef DEBUG
+#define TRACE(arg) (std::cout << arg << std::endl)
+#else
+#define TRACE(arg)
+#endif
+
+#endif	/* DEBUG_H */
+
