459160ebef5c8276d82fd4eb5a9f48971e347ab1
Jan Machala <jan.machala@email.cz>
2011-11-13 14:11:13 +0100

    Added TRACELN for debug printing with endl and TRACE for printing without endl

--- Debug.h
+++ Debug.h
@@ -9,9 +9,11 @@
 #define	DEBUG_H
 
 #ifdef DEBUG
-#define TRACE(arg) (std::cout << arg << std::endl)
+#define TRACE(arg) (std::cout << arg)
+#define TRACELN(arg) (std::cout << arg << std::endl)
 #else
 #define TRACE(arg)
+#define TRACELN(arg)
 #endif
 
 #endif	/* DEBUG_H */
